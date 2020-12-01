873077c134ea62410a4bc0b23f93ec3d53412e38
legkoszkur <65813842+legkoszkur@users.noreply.github.com>
2020-12-01 22:10:59 +0100

    Repair fault on backend

--- Class_statistical_backend.py
+++ Class_statistical_backend.py
@@ -29,10 +29,10 @@ class StatisticBackend:
                     sum(self.data[i]), np.mean(self.data[i]), max(self.data[i]), min(self.data[i]),
                     np.quantile(self.data[i], q=0.5), np.quantile(self.data[i], q=0.25),
                     np.quantile(self.data[i], q=0.75), self.data[i].mode(dropna=False)[0]])
-            self.average_measures_df = pd.DataFrame(
-                np.array(self.average_m_l).reshape(-1, len(self.input_var)),
-                index=self.average_m_id, columns=self.input_var)
+            self.average_measures_df = pd.DataFrame(np.transpose(self.average_m_l),index=self.average_m_id, columns=self.input_var)
             self.average_measures_df = self.average_measures_df.loc[self.input_stat]
+            self.average_measures_df = self.average_measures_df.round(decimals=1)
+
 
 
         elif self.wish == 1:
@@ -42,23 +42,20 @@ class StatisticBackend:
                     max(self.data[i]) - min(self.data[i]),
                     np.quantile(self.data[i], q=0.75) - np.quantile(self.data[i], q=0.25),
                     (np.quantile(self.data[i], q=0.75) - np.quantile(self.data[i], q=0.25)) / 2, ])
-            self.differentiation_measures_df = pd.DataFrame(
-                np.array(self.differentiation_m_l).reshape(-1, len(self.input_var)),
-                index=self.differentiation_m_id, columns=self.input_var)
+            self.differentiation_measures_df = pd.DataFrame(np.transpose(self.differentiation_m_l),
+                                                            index=self.differentiation_m_id, columns=self.input_var)
             self.differentiation_measures_df = self.differentiation_measures_df.loc[self.input_stat]
 
         elif self.wish == 2:
             for i in self.input_var:
                 self.skewness_l.append([skew(self.data[i])])  # Pearson
-            self.skewness_df = pd.DataFrame(
-                np.array(self.skewness_l).reshape(-1, len(self.input_var)),
-                index=self.skewness_id, columns=self.input_var)
+            self.skewness_df = pd.DataFrame(np.transpose(self.skewness_l),
+                                            index=self.skewness_id, columns=self.input_var)
             self.skewness_df = self.skewness_df.loc[self.input_stat]
 
         elif self.wish == 3:
             for i in self.input_var:
                 self.kurtosis_l.append([kurtosis(self.data[i])])  # Pearson?~
-            self.kurtosis_df = pd.DataFrame(
-                np.array(self.kurtosis_l).reshape(-1, len(self.input_var)),
-                index=self.kurtosis_id, columns=self.input_var)
+            self.kurtosis_df = pd.DataFrame(np.transpose(self.kurtosis_l,
+                index=self.kurtosis_id, columns=self.input_var))
             self.kurtosis_df = self.kurtosis_df.loc[self.input_stat]
--- brudnopisy/brudnopis1.py
+++ brudnopisy/brudnopis1.py
@@ -1,163 +1,69 @@
-import tkinter as tk
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from Function_message_window import popup_window
-from Class_statistical_backend import StatisticBackend
+import pandas as pd
+import numpy as np
+from scipy.stats import kurtosis, skew
 
 
-class Skewness:  # todo tutuaj będzie okienko do tworzenia wykresów
-    def __init__(self, master, data):
-        self.master = master
+class StatisticBackend:
+    def __init__(self, data, input_var,input_stat, wish,):
         self.data = data
-        self.master.geometry("1000x600")
-        self.master.resizable(False, False)
-
-        self.graph_f = tk.LabelFrame(self.master)
-        self.graph_f.place(relx=0.195, rely=0.01, relwidth=0.61, relheight=0.98)
-
-        self.menu_f = tk.LabelFrame(self.master, text="Menu")
-        self.menu_f.place(relx=0.81, rely=0.91, relwidth=0.185, relheight=0.08)
-
-        self.stat_lf = tk.LabelFrame(self.master, text="Skewness", relief="flat")
-        self.stat_lf.place(relx=0.005, rely=0.01, relwidth=0.185, relheight=0.37)
-
-        self.ch1 = tk.StringVar()
-        self.ch_b1 = tk.Checkbutton(self.stat_lf, text="Skewness", variable=self.ch1,
-                                    onvalue="Skewness", tristatevalue=0, )
-        self.ch_b1.grid(row=0, sticky="W")
-
-        self.quit_b = tk.Button(self.menu_f, text="Close", command=self.close_window)
-        self.quit_b.place(relx=0.1, rely=0.1, relwidth=0.3, relheight=0.8)
-
-        self.load_b = tk.Button(self.menu_f, text="Load", command=lambda: self.chosen_data_insert())
-        self.load_b.place(relx=0.5, rely=0.1, relwidth=0.3, relheight=0.8)
-
-        self.variables = list(data.columns)
-
-        self.text_lf1 = tk.LabelFrame(self.master, bg="white", text="Existing variables", relief="flat")
-        self.text_lf1.place(relx=0.81, rely=0.01, relwidth=0.185, relheight=0.44)
-        self.text_1 = tk.Text(self.text_lf1, bd=4, relief="groove", wrap="word")
-        # warp word powoduje że przenosi całe słowo do następnej linijki
-        self.text_1.place(relx=0.01, rely=0.01, relwidth=0.97, relheight=0.97)
-        self.text_1.configure(state='normal')
-        self.text_1.insert(tk.END, self.variables)
-        self.text_1.configure(state='disabled')
-
-        self.text_lf2 = tk.LabelFrame(self.master, bg="white", text="Variables on graph", relief="flat")
-        self.text_lf2.place(relx=0.81, rely=0.46, relwidth=0.185, relheight=0.44)
-        self.text_2 = tk.Text(self.text_lf2, bd=4, relief="groove", wrap="word")
-        self.text_2.place(relx=0.01, rely=0.01, relwidth=0.97, relheight=0.97)
-
-        self.options_lf = tk.LabelFrame(self.master, text="Options")
-        self.options_lf.place(relx=0.005, rely=0.91, relwidth=0.185, relheight=0.08)
-        self.ratio_var = tk.IntVar()
-        self.radio_b1 = tk.Radiobutton(self.options_lf, text="Report", value=1, variable=self.ratio_var)
-        self.radio_b2 = tk.Radiobutton(self.options_lf, text="Graph", value=0, variable=self.ratio_var)
-        self.radio_b1.grid(row=0, column=0, sticky="W")
-        self.radio_b2.grid(row=0, column=1, sticky="W")
-
-        self.widget = None
-        self.toolbar = None
-        self.text_stat = None
-
-    def close_window(self):
-        self.master.destroy()
-
-    def chosen_data_insert(self):
-        self.input_var = self.text_2.get("1.0", "end")
-        self.input_var = self.input_var.split(" ")
-        self.input_var = [x for x in self.input_var if x]  # to usuwa puste (w srodku ale nie na koncu)
-        self.input_var[-1] = self.input_var[-1].strip()  # to usuwa \n
-        if self.input_var[-1] == '':  # usuwa ostatnie puste miejsce jakby się pojawiło
-            self.input_var = self.input_var[:-1]
-        self.check_list = all(item in self.variables for item in self.input_var)
-
-        self.check_b_l = [self.ch1.get(),]
-        # to usuwa puste pola żeby można było załadować odpowiednie nazywy
-        self.check_b_l = [x for x in self.check_b_l if x]
-
-        if self.check_list is True:
-            # to sprawdza czy wszystkie wprowadzone zmienne sa poprawne
-
-            if self.ratio_var.get() == 0:
-                if self.widget:
-                    self.widget.destroy()
-
-                if self.toolbar:
-                    self.toolbar.destroy()
-
-                if self.text_stat:
-                    self.text_stat.destroy()
-
-                f = Figure(figsize=(5, 5), dpi=100)
-                a = f.add_subplot(111)
-                order_numbers = []
-
-                for i in range(len(self.data[self.input_var])):
-                    order_numbers.append(i)
-                a.plot(order_numbers, self.data[self.input_var])
-
-                canvas = FigureCanvasTkAgg(f, master=self.graph_f)
-                self.toolbar = NavigationToolbar2Tk(canvas, self.graph_f)
-                self.widget = canvas.get_tk_widget()
-                self.widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-            elif self.ratio_var.get() == 1:
-
-                if self.widget:
-                    self.widget.destroy()
-
-                if self.toolbar:
-                    self.toolbar.destroy()
-
-                if self.text_stat:
-                    self.text_stat.destroy()
-
-                self.statistical_backend = StatisticBackend(self.data, self.input_var, self.check_b_l, 2, )
-                self.text_stat = tk.Text(self.graph_f, bd=4, relief="groove", wrap="word")
-                self.text_stat.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-                self.text_stat.configure(state='normal')
-                self.text_stat.insert(tk.END, self.statistical_backend.skewness_df)
-                self.text_stat.configure(state='disabled')
-
-        else:
-            popup_window("Information", "Incorrect variable name!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.input_var = input_var
+        self.input_stat = input_stat
+        self.wish = wish
+
+        self.average_m_l = []
+        self.differentiation_m_l = []
+        self.skewness_l = []
+        self.kurtosis_l = []
+
+        self.average_m_id = ['Sum', 'Mean', "Max", "Min", "Median", "Quantile_25", "Quantile_75", "Dominant"]
+        self.differentiation_m_id = ["Standard deviation", "Coefficient of variation", "Range",
+                                            "Interquartile range", "Quarterly deviation", ]
+        self.skewness_id = ["Skewness"]
+        self.kurtosis_id = ["Kurtosis"]
+
+
+
+        if self.wish == 0:
+            for i in self.input_var:
+                self.average_m_l.append([
+                    sum(self.data[i]), np.mean(self.data[i]), max(self.data[i]), min(self.data[i]),
+                    np.quantile(self.data[i], q=0.5), np.quantile(self.data[i], q=0.25),
+                    np.quantile(self.data[i], q=0.75), self.data[i].mode(dropna=False)[0]])
+                print(self.average_m_l)#tu jest błąd bo on źle tworzy te kolumny
+            self.average_measures_df = pd.DataFrame(
+                np.array(self.average_m_l).reshape(-1, len(self.input_var)),
+                index=self.average_m_id, columns=self.input_var)
+            print(self.average_measures_df)
+            self.average_measures_df = self.average_measures_df.loc[self.input_stat]
+            print(self.average_measures_df)
+            self.average_measures_df = self.average_measures_df.round(decimals=1)
+            print(self.average_measures_df)
+
+
+        elif self.wish == 1:
+            for i in self.input_var:
+                self.differentiation_m_l.append([
+                    np.std(self.data[i]), np.std(self.data[i]) / np.mean(self.data[i]) * 100,
+                    max(self.data[i]) - min(self.data[i]),
+                    np.quantile(self.data[i], q=0.75) - np.quantile(self.data[i], q=0.25),
+                    (np.quantile(self.data[i], q=0.75) - np.quantile(self.data[i], q=0.25)) / 2, ])
+            self.differentiation_measures_df = pd.DataFrame(
+                np.array(self.differentiation_m_l).reshape(-1, len(self.input_var)),
+                index=self.differentiation_m_id, columns=self.input_var)
+            self.differentiation_measures_df = self.differentiation_measures_df.loc[self.input_stat]
+
+        elif self.wish == 2:
+            for i in self.input_var:
+                self.skewness_l.append([skew(self.data[i])])  # Pearson
+            self.skewness_df = pd.DataFrame(
+                np.array(self.skewness_l).reshape(-1, len(self.input_var)),
+                index=self.skewness_id, columns=self.input_var)
+            self.skewness_df = self.skewness_df.loc[self.input_stat]
+
+        elif self.wish == 3:
+            for i in self.input_var:
+                self.kurtosis_l.append([kurtosis(self.data[i])])  # Pearson?~
+            self.kurtosis_df = pd.DataFrame(
+                np.array(self.kurtosis_l).reshape(-1, len(self.input_var)),
+                index=self.kurtosis_id, columns=self.input_var)
+            self.kurtosis_df = self.kurtosis_df.loc[self.input_stat]
