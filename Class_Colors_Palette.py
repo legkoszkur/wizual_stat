3f5a03b6b34d0d77e5dee0e4231a9b5570acef2e
legkoszkur <65813842+legkoszkur@users.noreply.github.com>
2020-12-03 22:43:37 +0100

    nowa klasa do wyboru kolorów
    na jutro do dokonczenia nowa klasa do wyboru kolorów

---
+++ Class_Colors_Palette.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import colorchooser
+colors_list = ["red", "blue", "green", "brown", "yellow", "pink", "gold", "cyan"]
+input_var = ["x1","x2","x3","x4","x5","x6","x7","x8",]
+# todo ta klasa będzie zwracać wektor z nowymi kodami kolorów
+#https://www.youtube.com/watch?v=NDCirUTTrhg&ab_channel=Codemy.com
+#todo będzie przymować zmienne zmiennych zeby wiedzieć o ile kolorów zapytać
+#to będzie okno w którym będą przyciski w zależności od tego ile będzie zmiennych podanych tyle przycisków i pod kazdym przyciskiem będzie wyskakiwać
+
+class AskColors:
+    def __init__(self, input_v, colors_l):
+        self.input_var = input_v
+        self.color_list = colors_l
+
+
+
+
+
+
