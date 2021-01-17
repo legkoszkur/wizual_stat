a7f9f7eb517c6a61647f03cc1ee90ac00d717d6d
legkoszkur <65813842+legkoszkur@users.noreply.github.com>
2021-01-17 18:08:58 +0100

    LAST UPDATE
    Last update before publishing

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# wizual_stat
- Program do statystyki i wizualizacji
+# Program to data visualisation 
+
+
+## Goals
+My main goal was to learn how build desktop application. 
+During work on this project I learned basics of tkinter and matplotlib libraries.
+
+Moreover, working on this project has broadened my point of view in building applications.
+
+## Challenges
+
+On my way, I encountered challenges such as:
+1. Unreadable code (after breaks)
+2. Meany python errors (especially on the beginning)
+3. Some technical problems (laptop memory accident)
+4. Stagnation in the project (especially on the end)
+
+## Results
+
+In response to the challenges associated with the project, I expanded my knowledge to such issues as:
+
+1. PEP8 coding standards , OOP paradigm
+2. Fast collecting useful informations from the internet
+3. Use GIT repositories as safe and efficient space to work on project.
+4. Understanding the issues of SCRUM and AGILE in the context of project work and management.
+
+## Final word
+
+Some of the knowledge gained during the project was not fully implemented in it. This was due to the fact that some parts were written much later than others. In addition, I didn't want to stop at this project as I achieved my goals much more than I had initially assumed.
+
+## Technical issues 
+
+The project was implemented between 01.10.2020 to 17.01.2021.
+
+
+
--- window_instruction.py
+++ window_instruction.py
@@ -7,20 +7,34 @@ class InstructionWindow:
     def __init__(self, master, ):
 
         self.master = master
-        self.master.geometry("400x200")
+        self.master.geometry("400x400")
         self.master.resizable(False, False)
 
-        self.text = "To enter more than one variable names you must \n use one space as described on below example:"
-        self. example_variables = "x1 x2 x3"
+        self.text = "1. To enter more than one variable name you must use one space as described on below example:"
 
-        self.label = Label(self.master, text=self.text, justify='left', bg="white", )
-        self.label.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.2)
+        self.text_1 = Text(self.master, wrap="word" )
+        self.text_1.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.15)
+
+        self.text_1.configure(state='normal')
+        self.text_1.insert(END, self.text)
+        self.text_1.configure(state='disabled')
 
         self.text_lf1 = LabelFrame(self.master, text="Existing variables", relief="flat")
-        self.text_lf1.place(relx=0.35, rely=0.25, relwidth=0.3, relheight=0.7)
-        self.text_1 = Text(self.text_lf1, bd=4, relief="groove", wrap="word")
+        self.text_lf1.place(relx=0.35, rely=0.21, relwidth=0.3, relheight=0.5)
 
-        self.text_1.place(relx=0.01, rely=0.01, relwidth=0.97, relheight=0.97)
-        self.text_1.configure(state='normal')
-        self.text_1.insert(END, self.example_variables)
-        self.text_1.configure(state='disabled')
+        self.example_variables = "x1 x2 x3"
+
+        self.text_2 = Text(self.text_lf1, bd=4, relief="groove", wrap="word")
+        self.text_2.place(relx=0.01, rely=0.01, relwidth=0.97, relheight=0.97)
+        self.text_2.configure(state='normal')
+        self.text_2.insert(END, self.example_variables)
+        self.text_2.configure(state='disabled')
+
+        self.text2 = "2. In order for the data to be correctly imported, it should be in CSV, XLSX or XLS formats." \
+            " In addition, all variable names should be on the first row and assigned values vertically below them."
+
+        self.text_3 = Text(self.master, wrap="word")
+        self.text_3.place(relx=0.05, rely=0.7, relwidth=0.9, relheight=0.25)
+        self.text_3.configure(state='normal')
+        self.text_3.insert(END, self.text2)
+        self.text_3.configure(state='disabled')
