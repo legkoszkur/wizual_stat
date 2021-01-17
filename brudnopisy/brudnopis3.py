4a9cdee74da4c620a49347322a8aff9fc08efb6c
legkoszkur <65813842+legkoszkur@users.noreply.github.com>
2021-01-17 18:10:03 +0100

    Delete brudnopisy directory

--- brudnopisy/brudnopis3.py
+++
@@ -1,4 +0,0 @@
-from pandas import DataFrame
-d = [["M",10,2.5,12],["F",6,1.6,7],["M",9,2.2,9],["M",11,2.4,10],["F",7,1.9,9]]
-df = DataFrame(d, columns=['Gender','Lenght','Height','Weight'])
-print(df)
